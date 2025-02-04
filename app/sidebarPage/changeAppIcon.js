ee6d26343c74c98e2965c680d9374e34af830a35
OLAJESU BENJAMIN <benjamin.ola070@gmail.com>
2025-02-04 21:20:58 +0100

    remove unused React plugin from build.gradle; add new app icon images

--- app/sidebarPage/changeAppIcon.js
+++ app/sidebarPage/changeAppIcon.js
@@ -1,29 +1,108 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet, FlatList, Dimensions } from 'react-native';
-import { useNavigation } from '@react-navigation/native';  // Import for navigation
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native'; // Import for navigation
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 const icons = [
-  { id: 1, name: 'Classic', source: require('../../assets/images/appIcon1.png') },
-  { id: 2, name: 'Dark Elegance', source: require('../../assets/images/appIcon2.png') },
-  { id: 3, name: 'Minimalist White', source: require('../../assets/images/appIcon3.png') },
-  { id: 4, name: 'Retro Vibes', source: require('../../assets/images/appIcon4.png') },
-  { id: 5, name: 'Midnight Mode', source: require('../../assets/images/appIcon5.png') },
-  { id: 6, name: 'Ultra Minimal', source: require('../../assets/images/appIcon6.png') },
-  { id: 7, name: 'Futuristic', source: require('../../assets/images/appIcon1.png') },
-  { id: 8, name: 'Neon Glow', source: require('../../assets/images/appIcon2.png') },
+  {
+    id: 1,
+    name: 'Classic',
+    source: require('../../assets/images/appIcons/appIcon1.png'),
+  },
+  {
+    id: 2,
+    name: 'Dark Elegance',
+    source: require('../../assets/images/appIcons/appIcon2.png'),
+  },
+  {
+    id: 3,
+    name: 'Minimalist White',
+    source: require('../../assets/images/appIcons/appIcon3.png'),
+  },
+  {
+    id: 4,
+    name: 'Retro Vibes',
+    source: require('../../assets/images/appIcons/appIcon4.png'),
+  },
+  {
+    id: 5,
+    name: 'Midnight Mode',
+    source: require('../../assets/images/appIcons/appIcon5.png'),
+  },
+  {
+    id: 6,
+    name: 'Ultra Minimal',
+    source: require('../../assets/images/appIcons/appIcon6.png'),
+  },
+  {
+    id: 7,
+    name: 'Futuristic',
+    source: require('../../assets/images/appIcons/appIcon7.png'),
+  },
+  {
+    id: 8,
+    name: 'Neon Glow',
+    source: require('../../assets/images/appIcons/appIcon8.png'),
+  },
+  {
+    id: 9,
+    name: 'Cyberpunk',
+    source: require('../../assets/images/appIcons/appIcon9.png'),
+  },
+  {
+    id: 10,
+    name: 'Elegant Gold',
+    source: require('../../assets/images/appIcons/appIcon10.png'),
+  },
+  {
+    id: 11,
+    name: 'Nature Touch',
+    source: require('../../assets/images/appIcons/appIcon11.png'),
+  },
+  {
+    id: 12,
+    name: 'Gradient Shine',
+    source: require('../../assets/images/appIcons/appIcon12.png'),
+  },
+  {
+    id: 13,
+    name: 'Modern Blue',
+    source: require('../../assets/images/appIcons/appIcon13.png'),
+  },
+  {
+    id: 14,
+    name: 'Tech Silver',
+    source: require('../../assets/images/appIcons/appIcon14.png'),
+  },
+  {
+    id: 15,
+    name: 'Vibrant Pop',
+    source: require('../../assets/images/appIcons/appIcon15.png'),
+  },
+  {
+    id: 16,
+    name: 'Mystic Purple',
+    source: require('../../assets/images/appIcons/appIcon16.png'),
+  },
 ];
 
 export default function ChangeAppIcon() {
   const [selectedIcon, setSelectedIcon] = useState(icons[0]);
-   const navigation = useNavigation();  // Initialize the navigation object
-
+  const navigation = useNavigation(); // Initialize the navigation object
 
   const handleContinue = () => {
-  // Close the current page (go back to the previous screen)
-  navigation.goBack();
-};
+    // Close the current page (go back to the previous screen)
+    navigation.goBack();
+  };
 
   const handleIconChange = (icon) => {
     setSelectedIcon(icon);
@@ -57,7 +136,9 @@ export default function ChangeAppIcon() {
         />
       </View>
 
-      <Text style={styles.selectedText}>Selected: {selectedIcon.name}</Text>
+      <Text style={styles.selectedText}>
+        Selected App Icon Name: {selectedIcon.name}
+      </Text>
 
       {/* Continue Button */}
       <TouchableOpacity style={styles.continueButton} onPress={handleContinue}>
@@ -81,16 +162,15 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
   iconBox: {
-    width: '80%',
-    height: 300,
+    // width: '80%',
+    height: 450,
     backgroundColor: '#202325',
     alignItems: 'center',
   },
   iconContainer: {
-    width: '100%', 
+    width: '100%',
     justifyContent: 'center',
     // backgroundColor: 'blue',
-
   },
   iconWrapper: {
     alignItems: 'center',
@@ -98,8 +178,8 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   icon: {
-    width: 30,
-    height: 30,
+    width: 40,
+    height: 40,
   },
   selectedIcon: {
     borderWidth: 1,
